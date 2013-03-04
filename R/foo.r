e16eeda58291d050ccf65ec11d7504c56eda07b5
Michael <mnels@HAUDA0906TW.doh.health.nsw.gov.au>
2013-03-04 16:15:39 +1100

    a basic r package

---
+++ R/foo.r
@@ -0,0 +1,16 @@
+#' add a column by reference
+#' 
+#' @param x a data.table
+#' @import data.table
+#' @export
+foo <- function(x){
+  x[, {'a'} := 1]
+}
+#' foo with call
+#'
+#' @param x a data.table
+#' @export
+fooCall <- function(x){
+  eval(substitute(x[, a :=1]),parent.frame(1))
+} 
+
